35e3dc152c6f409f6d58de65f63ba24d71726798
satie <satiesharma@gmail.com>
2017-07-12 15:35:14 -0400

    Added getIPAddresses function to dynamodb library

--- test/dynamodb.test.js
+++ test/dynamodb.test.js
@@ -10,7 +10,7 @@ aws.config.setPromisesDependency(null);
 // test locally
 aws.config.update({
     region: "us-east-1",
-    endpoint: "http://localhost:8000"
+    endpoint: "http://localhost:4569"
 });
 
 describe("DynamoDB", function() {
@@ -23,12 +23,13 @@ describe("DynamoDB", function() {
         if (err) {
           done(err);
         } else {
-          // console.log(data);
+          // console.log(results);
           dynamodb.describeTable({TableName: "IPBlacklistTest"}, function (e, d) {
             if (e) {
               done(e);
             } else {
               expect(d.Table.TableName).to.equal("IPBlacklistTest");
+              expect(d.Table.StreamSpecification.StreamEnabled).to.be.true;
               done();
             }
           });
@@ -191,6 +192,86 @@ describe("DynamoDB", function() {
     });
 
     after(function(done) {
+      dynamodb.describeTable({TableName: "IPBlacklistTest"}, function (e, d) {
+        if (e) {
+          done(e);
+        } else {
+          dynamodb.deleteTable({TableName: "IPBlacklistTest"}, function(err, results) {
+            done(err);
+          });
+        }
+      });
+    });
+  });
+
+  describe("Retrieves blacklist records", function(done) {
+    var dynamodb = new aws.DynamoDB();
+    before("creates test database", function(done) {
+      var tableParams = {
+        TableName: "IPBlacklistTest",
+        KeySchema: [{
+          AttributeName: "IPAddress",
+          KeyType: "HASH"
+        }],
+        AttributeDefinitions: [{
+          AttributeName: "IPAddress",
+          AttributeType: "S"
+        }],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1000
+        },
+        StreamSpecification: {
+          StreamEnabled: true,
+          StreamViewType: "NEW_AND_OLD_IMAGES"
+        }
+      };
+      dynamodb.describeTable({TableName: "IPBlacklistTest"}, function (e, d) {
+        if (e) {
+          if (e.code === 'ResourceNotFoundException') {
+            // table does not exist. create it
+            dynamodb.createTable(tableParams, function(e, d) {
+              if (e) {
+                done(e);
+              } else {
+                dynamodb.waitFor('tableExists', {
+                  TableName: "IPBlacklistTest"
+                }, (err, dat) => {
+                  if (e) {
+                    done(e);
+                  } else {
+                    // create test data
+                    const a = ["10.10.0.1"];
+                    const s = "emergingthreats";
+                    const t = new IPDatabase("IPBlacklistTest", 1000, 10);
+
+                    t.updateAddresses(a, s, function(e, r) {
+                      done(e);
+                    });
+                  }
+                });
+              }
+            });
+          } else {
+            done(e);
+          }
+        } else {
+          done();
+        }
+      });
+    });
+
+    it("gets active blacklist records", function(done) {
+      const testdb = new IPDatabase("IPBlacklistTest", 1000, 10);
+      testdb.getActiveIPAddresses().then(function(addresses) {
+        expect(addresses).to.include("10.10.0.1");
+        done();
+      }).catch(function(error) {
+        done(error);
+      })
+    });
+
+    after("deletes test database", function(done) {
       dynamodb.describeTable({TableName: "IPBlacklistTest"}, function (e, d) {
         if (e) {
           done(e);
